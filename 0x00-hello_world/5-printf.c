a8708b401b0822c6672d39d1864a90f3c7c75e7b
Hardeygold205 <surajudeenhadiademola330@gmail.com>
2023-09-07 12:12:18 -0700

    C Hello World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+        /*
+         * main function to print using printf
+         *
+         * return 0 for success
+         */
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
