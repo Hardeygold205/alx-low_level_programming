7fcc663447f8bec29d5923965d81d1c7f744fe28
Hardeygold205 <surajudeenhadiademola330@gmail.com>
2023-09-07 17:47:30 -0700

    C Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 
-int main(void) 
+int main(void)
+
 {
-	/*
-	 * puts function to print
-	 *
-	 * return 0 for success
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
